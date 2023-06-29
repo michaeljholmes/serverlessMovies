820d7cea2a4b1b68f6d3d208115a157c6f684b14
Michael Holmes <michael.holmes@optix.co.uk>
2023-06-29 13:56:47 +0100

    header update

--- netlify/functions/submitReview.js
+++ netlify/functions/submitReview.js
@@ -4,12 +4,20 @@ export const handler = async (event) => {
         return {
             statusCode: 200,
             body: JSON.stringify({ message: "Thank you for your review!"}),
-            headers: { 'content-type': 'application/json', 'access-control-allow-origin': '*' }
+            headers: { 
+                'content-type': 'application/json',
+                'access-control-allow-origin': '*',
+                'access-control-allow-methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+                'access-control-allow-headers': 'X-Requested-With, content-type'
+
+            }
         }
     }
     return {
         headers: {
-            'access-control-allow-origin': '*'
+            'access-control-allow-origin': '*',
+            'access-control-allow-methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+            'access-control-allow-headers': 'X-Requested-With, content-type'
         },
         statusCode: 404
     }
