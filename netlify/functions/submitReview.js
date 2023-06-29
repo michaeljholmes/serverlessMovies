7f3bfbcf4cfa3b7257fe0e57c0426fd436412b2b
Michael Holmes <michael.holmes@optix.co.uk>
2023-06-29 14:03:21 +0100

    200 for options request

--- netlify/functions/submitReview.js
+++ netlify/functions/submitReview.js
@@ -1,5 +1,15 @@
 
 export const handler = async (event) => {
+    if (event.httpMethod === "OPTIONS") {
+        return {
+            headers: {
+                'access-control-allow-origin': '*',
+                'access-control-allow-methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+                'access-control-allow-headers': 'X-Requested-With, content-type'
+            },
+            statusCode: 200
+        }
+    }
     if (event.httpMethod === 'POST') {
         return {
             statusCode: 200,
